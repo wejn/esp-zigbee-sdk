6325f455870178bf9ba593b31b20bf9709e68e06
Michal Jirků <box@wejn.org>
2025-05-27 16:57:05 +0200

    Move tckey to secur, add both types

--- components/esp-zigbee-console/src/cli_cmd_bdb.c
+++ components/esp-zigbee-console/src/cli_cmd_bdb.c
@@ -294,33 +294,6 @@ exit:
     return ret;
 }
 
-static esp_err_t cli_nwk_tckey(esp_zb_cli_cmd_t *self, int argc, char **argv)
-{
-    struct {
-        arg_hex_t *tckey;
-        arg_lit_t *help;
-        arg_end_t *end;
-    } argtable = {
-        .tckey = arg_hexn(NULL, NULL, "<key128:KEY>", 1, 1, "trust center key, in HEX format"),
-        .help = arg_lit0(NULL, "help", "Print this help message"),
-        .end = arg_end(2),
-    };
-    esp_err_t ret = ESP_OK;
-
-    /* Parse command line arguments */
-    EXIT_ON_FALSE(argc > 1, ESP_OK, arg_print_help((void**)&argtable, argv[0]));
-    int nerrors = arg_parse(argc, argv, (void**)&argtable);
-    EXIT_ON_FALSE(nerrors == 0, ESP_ERR_INVALID_ARG, arg_print_errors(stdout, argtable.end, argv[0]));
-
-    EXIT_ON_FALSE(argtable.tckey->hsize[0] == 16, ESP_ERR_INVALID_ARG);
-    esp_zb_secur_TC_standard_distributed_key_set(argtable.tckey->hval[0]);
-
-exit:
-    arg_hex_free(argtable.tckey);
-    ESP_ZB_CLI_FREE_ARGSTRUCT(&argtable);
-    return ret;
-}
-
 static esp_err_t cli_nwk_childmax(esp_zb_cli_cmd_t *self, int argc, char **argv)
 {
     struct {
@@ -506,6 +479,54 @@ static esp_err_t cli_nwk_ed_scan(esp_zb_cli_cmd_t *self, int argc, char **argv)
                                      cli_nwk_ed_scan_cb);
 
 exit:
+    ESP_ZB_CLI_FREE_ARGSTRUCT(&argtable);
+    return ret;
+}
+
+/* Sub-commands of `secur` */
+
+static esp_err_t cli_secur_tc_key(esp_zb_cli_cmd_t *self, int argc, char **argv)
+{
+    struct {
+        arg_str_t *type;
+        arg_hex_t *tckey;
+        arg_lit_t *help;
+        arg_end_t *end;
+    } argtable = {
+        .type = arg_str0("t", "type", "<key:d|p>", "set the key type ('d'istributed, 'p'reconfigured). Default: d"),
+        .tckey = arg_hexn(NULL, NULL, "<key128:KEY>", 1, 1, "trust center key, in HEX format"),
+        .help = arg_lit0(NULL, "help", "Print this help message"),
+        .end = arg_end(2),
+    };
+    esp_err_t ret = ESP_OK;
+
+    /* Parse command line arguments */
+    EXIT_ON_FALSE(argc > 1, ESP_OK, arg_print_help((void**)&argtable, argv[0]));
+    int nerrors = arg_parse(argc, argv, (void**)&argtable);
+    EXIT_ON_FALSE(nerrors == 0, ESP_ERR_INVALID_ARG, arg_print_errors(stdout, argtable.end, argv[0]));
+
+    EXIT_ON_FALSE(argtable.tckey->hsize[0] == 16, ESP_ERR_INVALID_ARG);
+
+    if (argtable.type->count > 0) {
+        switch (argtable.type->sval[0][0]) {
+            case 'd':
+            case 'D':
+                esp_zb_secur_TC_standard_distributed_key_set(argtable.tckey->hval[0]);
+                break;
+            case 'p':
+            case 'P':
+                esp_zb_secur_TC_standard_preconfigure_key_set(argtable.tckey->hval[0]);
+                break;
+            default:
+                EXIT_ON_ERROR(ESP_ERR_INVALID_ARG, cli_output_line("secur tckey: invalid argument to option --type"));
+                break;
+        }
+    } else {
+        esp_zb_secur_TC_standard_distributed_key_set(argtable.tckey->hval[0]);
+    }
+
+exit:
+    arg_hex_free(argtable.tckey);
     ESP_ZB_CLI_FREE_ARGSTRUCT(&argtable);
     return ret;
 }
@@ -959,13 +980,15 @@ DECLARE_ESP_ZB_CLI_CMD(channel, cli_channel,,  "Get/Set 802.15.4 channels for ne
 DECLARE_ESP_ZB_CLI_CMD_WITH_SUB(network, "Network configuration",
     ESP_ZB_CLI_SUBCMD(type,     cli_nwk_type,     "Get/Set the network type"),
     ESP_ZB_CLI_SUBCMD(key,      cli_nwk_key,      "Get/Set the network key"),
-    ESP_ZB_CLI_SUBCMD(tckey,    cli_nwk_tckey,      "Set the trust center key"),
     ESP_ZB_CLI_SUBCMD(legacy,   cli_nwk_legacy,   "Enable/Disable legacy device support"),
     ESP_ZB_CLI_SUBCMD(childmax, cli_nwk_childmax, "Get/Set max children number"),
     ESP_ZB_CLI_SUBCMD(open,     cli_nwk_open,     "Open local network"),
     ESP_ZB_CLI_SUBCMD(close,    cli_nwk_close,    "Close local network"),
     ESP_ZB_CLI_SUBCMD(scan,     cli_nwk_scan,     "Scan for network"),
     ESP_ZB_CLI_SUBCMD(ed_scan,  cli_nwk_ed_scan,  "Scan for energy detect on channels"),
+);
+DECLARE_ESP_ZB_CLI_CMD_WITH_SUB(secur, "Security configuration",
+    ESP_ZB_CLI_SUBCMD(tckey,   cli_secur_tc_key,  "Set the trust center key"),
 );
 DECLARE_ESP_ZB_CLI_CMD_WITH_SUB(ic, "Install code configuration",
     ESP_ZB_CLI_SUBCMD(policy, cli_ic_policy, "Set install code policy"),
